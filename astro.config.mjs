82e95f40d1f2dcd139c05c51c0e0dfe7b844acc9
conamx <dndhkdusdl@gmail.com>
2026-04-03 17:05:37 +0900

    fix: remove netlify adapter for static build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
 import { defineConfig } from 'astro/config';
-import netlify from '@astrojs/netlify';
 
 export default defineConfig({
   output: 'static',
-  adapter: netlify(),
   site: 'https://shareinfo.co.kr',
   build: {
     inlineStylesheets: 'always',
